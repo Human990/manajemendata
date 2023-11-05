055338abfe4f538bc08b346cc1b0587b0b0db970
berlinwibi <berlinwibi@gmail.com>
2023-11-05 15:31:20 +0700

    Pencarian jabatan

--- app/Http/Controllers/AdminKota/JabatanController.php
+++ app/Http/Controllers/AdminKota/JabatanController.php
@@ -12,18 +12,50 @@ class JabatanController extends Controller
     public function index(Request $request)
     {
         $tahunid = session()->get('tahun_id_session');
-        $datas= Jabatan::select('jabatans.kode_jabatanlama AS id', 'jabatans.nama_jabatan', 'jabatans.nilai_jabatan', 'jabatans.indeks_id', 'jabatans.tunjab', 'master_tahun.tahun', 'indeks.kelas_jabatan', 'indeks.indeks', 'jenis_jabatans.jenis_jabatan')
-                     ->leftjoin('master_tahun', 'master_tahun.id', '=', 'jabatans.tahun_id')
-                     ->leftJoin('indeks', function ($join) use ($tahunid) {
-                        $join->on('indeks.kode_indeks', '=', 'jabatans.indeks_id');
-                        $join->on('indeks.tahun_id', '=', DB::raw($tahunid));
-                     })
-                     ->leftjoin('jenis_jabatans', 'jenis_jabatans.id', '=', \DB::raw('CAST(indeks.jenis_jabatan AS INTEGER)'))
-                     ->where('jabatans.tahun_id', session()->get('tahun_id_session'))
-                     ->orderBy('jabatans.created_at','DESC')
-                     ->paginate(10);
+        $pencarian = $request->pencarian;
 
-        return view('admin-kota.master.master-jabatan',compact('datas'));
+        if (!empty($request->pencarian)) {
+            $datas= Jabatan::select('jabatans.kode_jabatanlama AS id', 
+                                    'jabatans.nama_jabatan', 
+                                    'jabatans.nilai_jabatan', 
+                                    'jabatans.indeks_id', 
+                                    'jabatans.tunjab', 
+                                    'master_tahun.tahun', 
+                                    'indeks.kelas_jabatan', 
+                                    'indeks.indeks', 
+                                    'jenis_jabatans.jenis_jabatan')
+                            ->leftjoin('master_tahun', 'master_tahun.id', '=', 'jabatans.tahun_id')
+                            ->leftJoin('indeks', function ($join) use ($tahunid) {
+                                $join->on('indeks.kode_indeks', '=', 'jabatans.indeks_id');
+                                $join->on('indeks.tahun_id', '=', DB::raw($tahunid));
+                            })
+                            ->leftjoin('jenis_jabatans', 'jenis_jabatans.id', '=', \DB::raw('CAST(indeks.jenis_jabatan AS INTEGER)'))
+                            ->where('jabatans.tahun_id', session()->get('tahun_id_session'))
+                            ->where('jabatans.nama_jabatan', 'LIKE', '%'.$pencarian.'%')
+                            ->orderBy('jabatans.created_at','DESC')
+                            ->paginate(10);
+        }else {
+            $datas= Jabatan::select('jabatans.kode_jabatanlama AS id', 
+                                    'jabatans.nama_jabatan', 
+                                    'jabatans.nilai_jabatan', 
+                                    'jabatans.indeks_id', 
+                                    'jabatans.tunjab', 
+                                    'master_tahun.tahun', 
+                                    'indeks.kelas_jabatan', 
+                                    'indeks.indeks', 
+                                    'jenis_jabatans.jenis_jabatan')
+                            ->leftjoin('master_tahun', 'master_tahun.id', '=', 'jabatans.tahun_id')
+                            ->leftJoin('indeks', function ($join) use ($tahunid) {
+                                $join->on('indeks.kode_indeks', '=', 'jabatans.indeks_id');
+                                $join->on('indeks.tahun_id', '=', DB::raw($tahunid));
+                            })
+                            ->leftjoin('jenis_jabatans', 'jenis_jabatans.id', '=', \DB::raw('CAST(indeks.jenis_jabatan AS INTEGER)'))
+                            ->where('jabatans.tahun_id', session()->get('tahun_id_session'))
+                            ->orderBy('jabatans.created_at','DESC')
+                            ->paginate(10);
+        }
+
+        return view('admin-kota.master.master-jabatan',compact('datas', 'pencarian'));
     }
 
     public function store(Request $request)
