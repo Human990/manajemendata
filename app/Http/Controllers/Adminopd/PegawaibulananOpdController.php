315ef2898bfec42e5c7edb1955fb0a09a5b4cfcd
Human990 <singleuser69@gmail.com>
2023-11-07 15:31:30 +0700

    admin opd layout

--- app/Http/Controllers/Adminopd/PegawaibulananOpdController.php
+++ app/Http/Controllers/Adminopd/PegawaibulananOpdController.php
@@ -15,38 +15,44 @@ use Illuminate\Support\Facades\Auth;
 
 class PegawaibulananOpdController extends Controller
 {
-    // public function index(Request $request)
-    // {
-    //     $user = Auth::user();
-    //     $rupiah1 = Rupiah::where('id',1)->first();
-    //     $rupiah2 = Rupiah::where('id',2)->first();
-    //     $pegbul = Pegawai::with('jabatanbaru')->where('opd_id',$user->opd_id)->get();
-    //     $jumlah_pegbul = $pegbul->count();
-    //     $jabatanbaru = Jabatan::all();
-    //     return view('admin-opd.laporan.tpp-pegawai',compact(['pegbul','jumlah_pegbul','rupiah1','rupiah2','request']))->with('i',($request->input('page',1)-1));
-    // }
 
     public function index(Request $request)
     {
         $search = $request->input('search');
-        $datas = Pegawai::select(
-            'pegawais.*',
-            'master_tahun.tahun',
-            'jabatans.nama_jabatan',
-            'opds.nama_opd'
-        )
-        ->leftJoin('master_tahun', 'master_tahun.id', '=', 'pegawais.tahun_id')
-        ->leftJoin('jabatans', 'jabatans.kode_jabatanlama', '=', 'pegawais.kode_jabatanlama')
-        ->leftJoin('opds', 'opds.id', '=', 'pegawais.opd_id')
-        ->where('pegawais.tahun_id', session()->get('tahun_id_session'))
-        ->when($search, function ($query) use ($search) {
-            $query->where('nama_pegawai', 'LIKE', '%' . $search . '%')
-                  ->orWhere('nip', 'LIKE', '%' . $search . '%');
-        })
-        ->orderBy('pegawais.id', 'ASC')
-        ->paginate(10);
 
-    return view('admin-opd.laporan.tpp-pegawai', compact('datas','search'));
+        $datas = Pegawai::select(
+                'pegawais.id',
+                'pegawais.nip', 
+                'pegawais.nama_pegawai', 
+                'pegawais.sts_pegawai', 
+                'pegawais.sts_jabatan', 
+                'pegawais.pangkat', 
+                'pegawais.eselon', 
+                'pegawais.pensiun', 
+                'pegawais.tpp_tambahan', 
+                'pegawais.jumlah_pemangku', 
+                'pegawais.basic_tpp', 
+                'pegawais.total_bulan_penerimaan', 
+                'pegawais.opd_id', 
+                'jabatans.kode_jabatanlama', 
+                'jabatans.nama_jabatan', 
+                'jabatans.nilai_jabatan', 
+                'jabatans.indeks_id', 
+                'jabatans.tunjab', 
+                'master_tahun.tahun', 
+                'indeks.kelas_jabatan', 
+                'indeks.indeks', 
+                'jenis_jabatans.jenis_jabatan'
+            )
+            ->leftJoin('master_tahun', 'master_tahun.id', '=', 'pegawais.tahun_id')
+            ->leftJoin('jabatans', 'jabatans.kode_jabatanlama', '=', 'pegawais.kode_jabatanlama')
+            ->leftJoin('indeks', 'indeks.kode_indeks', '=', 'jabatans.indeks_id')
+            ->leftJoin('jenis_jabatans', 'jenis_jabatans.id', '=', 'indeks.jenis_jabatan_id')
+            ->orderBy('pegawais.id', 'ASC')
+            ->where('pegawais.opd_id', Auth::user()->opd_id)  // Menyaring data berdasarkan opd dari user yang login
+            ->paginate(10);
+
+        return view('admin-opd.laporan.tpp-pegawai', compact('datas', 'search'));
     }
 
     public function putsession(Request $request)
